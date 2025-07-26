545b45f9cdd70a9ab12c1ec25c96ccf1b7d7a912
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-26 21:58:05 +0200

    feat: add functions to load default friends and posts

--- initialData.js
+++ initialData.js
@@ -1,3 +1,33 @@
 import { friendCreator, postCreator } from './socialCreators.js';
 import { domElements } from './dom.js';
 import { friends, posts } from './input.js';
+
+export function loadDefaultFriends(manager) {
+  if (manager.getFriends().length > 0) return;
+  const friendsArray = friends;
+  friendsArray.forEach(function (friend) {
+    const newFriend = friendCreator(friend.image, friend.alt, friend.name);
+    manager.pushFriend(newFriend);
+    makeFriend(
+      newFriend.getImage(),
+      newFriend.getAlternativText(),
+      newFriend.getFriendName()
+    );
+  });
+}
+
+//posts
+export function loadDefaultPosts(manager) {
+  if (manager.getPosts().length > 0) return;
+  const postsArray = posts;
+  postsArray.forEach(function (post) {
+    const newPost = postCreator(post.text, post.whoLike, post.commentsNumber);
+    manager.pushPost(newPost);
+    makePost(
+      newPost.getPostId(),
+      newPost.getPostText(),
+      newPost.getWhoLikePost(),
+      newPost.getPostCommentsNumber()
+    );
+  });
+}
