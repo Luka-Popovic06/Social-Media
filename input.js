c4d559e8b7430cc79840fb762516c0e48fe77dfc
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-26 22:09:13 +0200

    feat: add initial friends and posts data

---
+++ input.js
@@ -0,0 +1,49 @@
+export const friends = [
+  {
+    image: 'marc-andreas.jpg',
+    alt: 'Marc Andreas img',
+    name: 'Marc Andreas',
+  },
+  {
+    image: 'profile5.jpg',
+    alt: 'Ethan Turner img',
+    name: 'Ethan Turner',
+  },
+  {
+    image: 'profile4.jpg',
+    alt: 'Majda Odzaklijevska img',
+    name: 'Majda Odzaklijevska',
+  },
+  {
+    image: 'profile3.jpg',
+    alt: 'Kilibarda Petrovska img',
+    name: 'Kilibarda Petrovska',
+  },
+  {
+    image: 'profile2.jpg',
+    alt: 'Hiroshi Tanaka img',
+    name: 'Hiroshi Tanaka',
+  },
+  {
+    image: 'profile1.jpg',
+    alt: 'Gordana Stouns img',
+    name: 'Gordana Stouns',
+  },
+];
+export const posts = [
+  {
+    text: "If I'm to choose between one evil and another, I'd rather not choose at all.",
+    whoLike: 'Kilibarda Petrovska, Hiroshi Tanaka and 2 others likes this post',
+    commentsNumber: '4',
+  },
+  {
+    text: "It's not who I am underneath, but what I do that defines me.",
+    whoLike: 'Ethan Turner likes this post',
+    commentsNumber: '0',
+  },
+  {
+    text: "A true hero isn't measured by the size of his strength but by the strength of his heart",
+    whoLike: 'Gordana Stouns and Hiroshi Tanaka likes this post',
+    commentsNumber: '3',
+  },
+];
