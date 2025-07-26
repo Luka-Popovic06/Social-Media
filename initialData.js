0ef2eb973f2edc876bd9af08527435d39121471c
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-26 21:47:03 +0200

    Add loading spinner animation

--- initialData.js
+++ initialData.js
@@ -1,1 +1,3 @@
-
+import { friendCreator, postCreator } from './socialCreators.js';
+import { domElements } from './dom.js';
+import { friends, posts } from './input.js';
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+import { domElements } from './dom.js';
+import {
+  loadDefaultFriends,
+  makeFriend,
+  loadDefaultPosts,
+  makePost,
+} from './initialData.js';
+import { friendCreator, socialManager } from './socialCreators.js';
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    domElements.loader.classList.add('hidden');
+    domElements.nav.classList.remove('hidden');
+    domElements.main.classList.remove('hidden');
+  }, 2000);
+});
