46b3b3131fab0d74902551286c42139c365f4d4b
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-30 22:08:22 +0200

    Add toggle for comments and like functionality on posts

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ const manager = socialManager();
 loadDefaultFriends(manager);
 loadDefaultPosts(manager);
 //manager.getPosts().forEach(post => console.log(post.getCommentsArray()));
+
+domElements.postsList.addEventListener('click', function (e) {
+  if (e.target.closest('.comments-paragraph')) {
+    const li = e.target.closest('.post-item');
+    const comment = li.querySelector('.comments-list');
+    comment.classList.toggle('hidden');
+  } else if (e.target.closest('.like-btn')) {
+    const btn = e.target.closest('.like-btn');
+    const selectBtn = document.getElementById(btn.id);
+    selectBtn.style.backgroundColor = '#6063a0';
+    selectBtn.style.color = '#fff';
+    //
+    const postS = selectBtn.closest('.post-item');
+    manager.findPost(postS.id);
+    const selectP = manager.getSelectPost();
+    //
+    selectP.pushLike('Luka Popović');
+    console.log(selectP.getLikes());
+  }
+});
