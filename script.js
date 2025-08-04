7218873ebbb7cb8391b58188f0833c3acc97a008
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-04 22:05:17 +0200

    update: improve comment creation with user details

--- script.js
+++ script.js
@@ -90,3 +90,42 @@ domElements.postForm.addEventListener('submit', function (e) {
 });
 
 //Dodavanje komentara
+domElements.postsList.addEventListener('submit', function (e) {
+  if (!e.target.classList.contains('comment-form')) return;
+
+  e.preventDefault();
+
+  const form = e.target;
+  const input = form.querySelector('.write-comment-input');
+  const commentText = input.value;
+  if (!commentText) return;
+  const postElement = form.closest('.post-item');
+  const postId = postElement.id;
+
+  manager.findPost(postId);
+  const selectedPost = manager.getSelectPost();
+
+  const newComment = commentCreator(
+    userName.image,
+    userName.firstName,
+    userName.lastName,
+    commentText,
+    userName.defoltArray
+  );
+  selectedPost.pushComment(newComment);
+  makeComment(
+    postId,
+    newComment.getId(),
+    newComment.getImage(),
+    newComment.getFirstName(),
+    newComment.getLastName(),
+    newComment.getText(),
+    newComment.getLikesArray()
+  );
+  const commentNumber = postElement.querySelector('.comments-number');
+  commentNumber.textContent = selectedPost.getComments().length;
+  input.value = '';
+  const li = e.target.closest('.post-item');
+  const comment = li.querySelector('.comments-list');
+  comment.classList.remove('hidden');
+});
