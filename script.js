d3038bb526c18cf2f6202d946a18b1ece8eb4f3a
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-05 22:00:37 +0200

    Update like logic for posts and comments

--- script.js
+++ script.js
@@ -55,4 +55,102 @@ domElements.postsList.addEventListener('click', function (e) {
     const likesParagraph = postElement.querySelector('.likes-paragraph');
     likesParagraph.textContent = selectedPost.getWhoLikePost();
     //comment likes
-  } 
+  } else if (e.target.closest('.like-btn_comment')) {
+    const likeBtn = e.target.closest('.like-btn_comment');
+    const commentElement = likeBtn.closest('.comment-item');
+    const commentId = commentElement.id;
+
+    const postElement = likeBtn.closest('.post-item');
+    const post = postElement.id;
+    manager.findPost(post);
+    const selectedPost = manager.getSelectPost();
+    selectedPost.findComment(commentId);
+    const selectComment = selectedPost.getComment();
+    if (
+      selectComment
+        .getLikesArray()
+        .find(
+          like =>
+            like.firstName === userName.firstName &&
+            like.lastName === userName.lastName
+        )
+    ) {
+      likeBtn.style.backgroundColor = '#fff';
+      selectComment.getLikesArray().pop();
+    } else {
+      likeBtn.style.backgroundColor = '#6063a0';
+      selectComment.getLikesArray().push(userName.likesArray[0]);
+    }
+    const likesDisplay = commentElement.querySelector('.likes-count');
+    const whoLikeBox = commentElement.querySelector('.whoLike');
+    console.log(selectComment.getLikesArray());
+    likesDisplay.textContent = `${selectComment.getLikesArray().length} likes`;
+    selectComment.showLikesOnScrean(selectComment.getLikesArray());
+    whoLikeBox.textContent = selectComment.getWhoLikeComment();
+  }
+});
+
+domElements.postSubmitBtn.addEventListener('click', function () {
+  domElements.postForm.reset();
+});
+
+domElements.postForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const createdDate = Date();
+
+  const newPost = postCreator(postText);
+  manager.pushPost(newPost);
+  newPost.formatLikes(newPost.getLikes());
+  makePost(
+    newPost.getPostId(),
+    newPost.getPostText(),
+    newPost.getWhoLikePost(),
+    newPost.getComments().length
+  );
+  const post = document.getElementById(newPost.getPostId());
+  const dateParagraph = post.querySelector('.post-date');
+  dateParagraph.textContent = createdDate;
+});
+
+//Dodavanje komentara
+domElements.postsList.addEventListener('submit', function (e) {
+  if (!e.target.classList.contains('comment-form')) return;
+
+  e.preventDefault();
+
+  const form = e.target.closest('.comment-form');
+  console.log(form);
+  const input = form.querySelector('.write-comment-input');
+  const commentText = input.value;
+  if (!commentText) return;
+  const postElement = form.closest('.post-item');
+  const postId = postElement.id;
+
+  manager.findPost(postId);
+  const selectedPost = manager.getSelectPost();
+
+  const newComment = commentCreator(
+    userName.image,
+    userName.firstName,
+    userName.lastName,
+    commentText,
+    []
+  );
+  selectedPost.pushComment(newComment);
+  makeComment(
+    postId,
+    newComment.getId(),
+    newComment.getImage(),
+    newComment.getFirstName(),
+    newComment.getLastName(),
+    newComment.getText(),
+    newComment.getLikesArray().length,
+    newComment.getWhoLikeComment()
+  );
+  const commentNumber = postElement.querySelector('.comments-number');
+  commentNumber.textContent = selectedPost.getComments().length;
+  input.value = '';
+  const li = e.target.closest('.post-item');
+  const comment = li.querySelector('.comments-list');
+  comment.classList.remove('hidden');
+});
